7628a0d9a62c756459d5cb815ef2a06800f056a0
cjpannila <chinthanapannila@gmail.com>
2023-04-03 07:09:53 +1000

    Input file reading from args

--- src/main/java/com/fabricgroup/waterbillmanager/Main.java
+++ src/main/java/com/fabricgroup/waterbillmanager/Main.java
@@ -5,5 +5,13 @@ import com.fabricgroup.waterbillmanager.util.Constants;
 public class Main {
     public static void main(String[] args) {
         System.out.println(Constants.APPLICATION_START_MESSAGE);
+        if (args.length > 0) {
+            for (String arg : args) {
+                String inputFilePath = arg;
+                System.out.println(inputFilePath);
+            }
+        } else {
+            System.out.println(Constants.INPUT_FILEPATH_ARG_NOT_FOUND_MESSAGE);
+        }
     }
 }
--- src/main/java/com/fabricgroup/waterbillmanager/util/Constants.java
+++ src/main/java/com/fabricgroup/waterbillmanager/util/Constants.java
@@ -2,4 +2,5 @@ package com.fabricgroup.waterbillmanager.util;
 
 public class Constants {
     public final static String APPLICATION_START_MESSAGE = "### Water Bill manager application starting ###";
+    public final static String INPUT_FILEPATH_ARG_NOT_FOUND_MESSAGE = "Input file path argument not found";
 }
